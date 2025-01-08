d3f628cb4c6696071e5e8d2519678a51091eb8c2
sollunium <sollunium@github.com>
2025-01-07 20:06:28 -0500

    fixed error

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // basePath: "/sirun",
+  basePath: "/sirun",
   output: "export",
   reactStrictMode: false
 };
