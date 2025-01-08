51982d677ed6787c4d36e5748e7b2b7b7617e217
sollunium <sollunium@github.com>
2025-01-07 23:22:17 -0500

    fixed logo link

--- components/ui/nav.tsx
+++ components/ui/nav.tsx
@@ -22,7 +22,7 @@ const Nav = () => {
       <header>
         <nav className="flex items-center justify-between p-2 md:px-8" aria-label="Global">
           <div className="flex md:flex-1">
-            <Link href={`${basePath}`} className="-m-1.5 p-1.5">
+            <Link href='/' className="-m-1.5 p-1.5">
               <span className="sr-only">Sirunde</span>
               <img
                 className="h-8 w-auto"
@@ -64,7 +64,7 @@ const Nav = () => {
           <div className="fixed inset-0 z-50" />
           <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
-              <Link href={`${basePath}`} className="-m-1.5 p-1.5">
+              <Link href='/' className="-m-1.5 p-1.5">
                 <span className="sr-only">Main Logo</span>
                 <img
                   className="h-8 w-auto"
